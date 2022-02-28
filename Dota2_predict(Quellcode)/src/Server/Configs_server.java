795b13acb11cb7284795d8207bbc2d6e9da33150
FearPingWin <33061096+FearPingWin@users.noreply.github.com>
2022-02-28 16:30:36 +0200

    Update Configs_server.java
    hid data

--- Dota2_predict(Quellcode)/src/Server/Configs_server.java
+++ Dota2_predict(Quellcode)/src/Server/Configs_server.java
@@ -4,10 +4,10 @@ package Server;
  * Host, Port, Benutzer, Passwort, Name der Datenbank
  */
 public class Configs_server {
-	protected static String dbHost="pingwin91.beget.tech";
-	protected static String dbPort="3306";
-	protected static String dbUser="pingwin91_beleg";
-	protected static String dbPass="1P%or%Ag";
-	protected static String dbName="pingwin91_beleg";	
+	protected static String dbHost="****.beget.tech";
+	protected static String dbPort="****";
+	protected static String dbUser="****";
+	protected static String dbPass="****";
+	protected static String dbName="****";	
 	
 }
